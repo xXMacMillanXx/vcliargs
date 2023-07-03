88e0ead4c67345e29821f72aec0bfc70e8d6555b
xXMacMillanXx <xxmacmillanxx@gmx.at>
2023-07-03 19:55:17 +0200

    simple construction of data with test values

--- src/main.v
+++ src/main.v
@@ -4,6 +4,10 @@ import vcliargs as vc
 
 fn main() {
 	mut x := vc.Args.new('Arg Tester', 'This is testing the module', 'Don\'t know what should be here.')
-	x.add_argument(['-c', '--count'], 'Counts something.')
+	x.add_key('-c', ['--count'], 'Counts something.')
+	y := x.parse()
+	for key in y.keys() {
+		println(key + " ... " + y[key])
+	}	
 	x.print_help()
 }
--- src/vcliargs.v
+++ src/vcliargs.v
@@ -1,26 +1,80 @@
 module vcliargs
 
-// import os
+import os
+
+struct Key {
+	value string
+	description string
+mut:
+	alias []string
+}
+
+fn Key.new(key string, description string) Key {
+	return Key { value: key, description: description }
+}
+
+fn (mut k Key) add_alias(alias string) {
+	if alias in k.alias {
+		return
+	}
+
+	k.alias << alias
+}
+
+fn (k Key) is_key(test string) bool {
+	if test == k.value { return true }
+	if test in k.alias { return true }
+	
+	return false
+}
 
 struct Args {
 	texts []string
 mut:
-	args [][]string
-	descs []string
-	// values map[string]string
+	keys []Key
 }
 
-pub fn Args.new(prog string, description string, epilog string) Args {
-	return Args { texts: [prog, description, epilog] }
+pub fn Args.new(name string, desc string, epi string) Args {
+	mut x := Args { texts: [name, desc, epi] }
+	x.add_key('-h', ['--help'], 'Shows the help text.')
+
+	return x
 }
 
-pub fn (mut a Args) add_argument(arg []string, desc string) {
-	a.args << arg
-	a.descs << desc
+pub fn (mut a Args) add_key(key string, alias []string, description string) {
+	mut new_key := Key.new(key, description)
+	for x in alias {
+		new_key.add_alias(x)
+	}
+
+	a.keys << new_key
 }
 
-pub fn (a Args) parse() {
-	// todo
+fn (a Args) get_all_keys() []string {
+	mut keys := []string{}
+	for key in a.keys {
+		keys << key.value
+		for al in key.alias {
+			keys << al
+		}
+	}
+
+	return keys
+}
+
+pub fn (mut a Args) parse() map[string]string {
+	mut ret := map[string]string{}
+	a.keys.sort(a.value < b.value)
+
+	for key in a.keys {
+		for arg in os.args {
+			if key.is_key(arg) {
+				ret[arg] = "test"
+			}
+		}
+	}
+
+	return ret
 }
 
 pub fn (a Args) print_help() {
@@ -29,11 +83,12 @@ pub fn (a Args) print_help() {
 	println(a.texts[1]) // program description
 	println('')
 	println('Options')
-	for i, arglist in a.args {
-		for arg in arglist {
-			print(arg + " ")
+	for key in a.keys {
+		print(key.value + " ")
+		for al in key.alias {
+			print(al + " ")
 		}
-		print(a.descs[i] + '\n')
+		print(key.description + '\n')
 	}
 	println('')
 	println(a.texts[2]) // program epilog
