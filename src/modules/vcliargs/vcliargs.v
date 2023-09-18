cb3a5a72c360025d80865410460f14675db5f606
xXMacMillanXx <xxmacmillanxx@gmx.at>
2023-09-18 16:57:03 +0200

    preperation for tests

--- src/modules/vcliargs/vcliargs.v
+++ src/modules/vcliargs/vcliargs.v
@@ -6,10 +6,12 @@ struct Args {
 	texts []string
 mut:
 	keys []Key
+	args []string
 }
 
 pub fn Args.new(name string, desc string, epi string) Args {
 	mut x := Args { texts: [name, desc, epi] }
+	x.args = os.args
 	x.add_key(x.key('help', 'Shows the help text.').alias(['-h', '--help']).valueless(true))
 
 	return x
@@ -39,6 +41,11 @@ fn (a Args) get_key(s string) ?Key {
 		}
 	}
 	return none
+}
+
+// used for tests
+fn (mut a Args) set_args(s []string) {
+	a.args = s
 }
 
 fn (a Args) is_key(s string) bool {
@@ -92,7 +99,7 @@ pub fn (mut a Args) parse() map[string]string {
 	a.keys.sort(a.alias[0] < b.alias[0])
 	h := a.get_key_alias('help')
 	for v in h {
-		if v in os.args {
+		if v in a.args {
 			a.print_help()
 			exit(0)
 		}
@@ -109,7 +116,7 @@ pub fn (mut a Args) parse() map[string]string {
 		if key.is_required {
 			mut con := false
 			for v in key.alias {
-				if v in os.args || key.uses_default {
+				if v in a.args || key.uses_default {
 					con = true
 				}
 			}
@@ -120,35 +127,35 @@ pub fn (mut a Args) parse() map[string]string {
 		}
 	}
 
-	for i, arg in os.args {
+	for i, arg in a.args {
 		if a.is_key(arg) {
 			key := a.get_key(arg) or { panic(err) }
 			if key.is_valueless {
 				ret[key.value] = key.value
 				continue
 			}
-			if i+1 >= os.args.len {
+			if i+1 >= a.args.len {
 				ret[key.value] = ''
 				check_option(key, '')
 				check_type(key, '')
 				break
 			}
-			if a.is_key(os.args[i+1]) {
+			if a.is_key(a.args[i+1]) {
 				println('The parameter ${key.alias} requires a value, but received none.')
 				exit(1)
 			}
 			mut ii := 1
-			for !a.is_key(os.args[i+ii]) {
-				check_option(key, os.args[i+ii])
-				check_type(key, os.args[i+ii])
+			for !a.is_key(a.args[i+ii]) {
+				check_option(key, a.args[i+ii])
+				check_type(key, a.args[i+ii])
 				ii++
-				if i+ii >= os.args.len { break }
+				if i+ii >= a.args.len { break }
 			}
 			if !key.contains_multiple && ii > 2 {
-				println('The parameter ${key.alias} does not accept multiple values, but received ${os.args[i+1..i+ii]}. Only first value will be accepted.')
+				println('The parameter ${key.alias} does not accept multiple values, but received ${a.args[i+1..i+ii]}. Only first value will be accepted.')
 				ii = 2
 			}
-			ret[key.value] = os.args[i+1..i+ii].join(';')
+			ret[key.value] = a.args[i+1..i+ii].join(';')
 		}
 	}
 
