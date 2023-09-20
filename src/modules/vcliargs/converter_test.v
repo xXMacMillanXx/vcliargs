7e1b32cbadde795cb203e7414cc1c921348d8c9a
xXMacMillanXx <xxmacmillanxx@gmx.at>
2023-09-20 22:27:54 +0200

    write basic tests for converter and keys

--- src/modules/vcliargs/converter_test.v
+++ src/modules/vcliargs/converter_test.v
@@ -1,5 +1,31 @@
 module vcliargs
 
-fn test_convert() {
+fn test_convert_intended() {
+	assert 'abc' == convert[string]('abc') or { '' }
+
+	assert 5 == convert[int]('5') or { 0 }
+	assert 5 == convert[int]('5.5') or { 0 }
+	assert 0 == convert[int]('hello') or { 5 }
+	assert 0 == convert[int]('true') or { 5 }
 	
+	assert 12.34 == convert[f64]('12.34') or { 0.0 }
+	assert 5.0 == convert[f64]('5') or { 0.0 }
+	assert 0.0 == convert[f64]('hello') or { 12.34 }
+	assert 0.0 == convert[f64]('true') or { 5.0 }
+
+	assert true == convert[bool]('true') or { false }
+	assert false == convert[bool]('false') or { true }
+	assert false == convert[bool]('hello') or { true }
+	assert false == convert[bool]('1') or { true }
+	assert false == convert[bool]('5.5') or { true }
+}
+
+fn test_convert_negatives() {
+	assert 5 != convert[int]('five') or { 5 }
+	assert -5 == convert[int]('-5') or { 0 }
+
+	assert 5.0 != convert[f64]('five') or { 5.0 }
+	assert -5.0 == convert[f64]('-5.0') or { 0.0 }
+
+	assert true != convert[bool]('1') or { true }
 }
--- src/modules/vcliargs/key_test.v
+++ src/modules/vcliargs/key_test.v
@@ -1,5 +1,56 @@
 module vcliargs
 
 fn test_key() {
-	
+	mut k_test := Key.new('test', 'Test description')
+	mut k_control := Key { value: 'test', description: 'Test description' }
+
+	assert k_test == k_control
+
+	k_test = k_test.alias(['-t', '--test'])
+	k_control.alias = ['-t', '--test']
+
+	assert k_test == k_control
+
+	k_test = k_test.default('testing')
+	k_control.uses_default = true
+	k_control.default = 'testing'
+
+	assert k_test == k_control
+
+	k_test = k_test.valueless(true)
+	k_control.is_valueless = true
+
+	assert k_test == k_control
+
+	k_test = k_test.multiple(true)
+	k_control.contains_multiple = true
+
+	assert k_test == k_control
+
+	k_test = k_test.options(['ABC', 'testing', 'XYZ'])
+	k_control.uses_options = true
+	k_control.options = ['ABC', 'testing', 'XYZ']
+
+	assert k_test == k_control
+
+	k_test = k_test.type_check(ArgTypes.float)
+	k_control.type_checker = true
+	k_control.check_type = ArgTypes.float
+
+	assert k_test == k_control
+
+	k_test = k_test.required(true)
+	k_control.is_required = true
+
+	assert k_test == k_control
+
+	assert true == k_test.is_key('-t')
+	assert true == k_test.is_key('--test')
+	assert false == k_test.is_key('-t1')
+
+	assert true == k_test.is_valid_option('testing')
+	assert false == k_test.is_valid_option('QTP')
+
+	assert k_test.gen_usage() == '-t|--test'
+	assert k_test.gen_help() == '-t --test                                           Test description'
 }
